80c65e7a34efcbf2d32e73682166e7b442131386
volleAB <724958674@qq.com>
2018-08-25 21:49:30 +0800

    Continue

--- src/pages/mine.vue
+++ src/pages/mine.vue
@@ -8,13 +8,13 @@
       <div class="border"></div>
       <div class="user-info">
         <div class="user-base">基本资料</div>
-        <div class="user-password">密码修改</div>
+        <div class="user-base">密码修改</div>
         <div class="border"></div>
-        <div class="user-order">我的订单</div>
+        <div class="user-base">我的订单</div>
         <div class="border"></div>
       </div>
-      <div class="problem">常见问题</div>
-      <div class="aboutus">关于我们</div>
+      <div class="user-base">常见问题</div>
+      <div class="user-base">关于我们</div>
     </div>
   </div>
 </template>
